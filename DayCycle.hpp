8db6ccf646faabf408f36e5a8f6757701477409e
tigran <tyavroyan@gmail.com>
2024-02-10 14:32:55 +0400

    finally done

--- DayCycle.hpp
+++ DayCycle.hpp
@@ -5,22 +5,19 @@
 #include <fstream>
 #include <iostream>
 #include "Earth.hpp"
-#include <string>
-
-using std::string;
 
 class DayCycle {
 public:
     void operator ()(std::ofstream& file, Earth& earth, int day = 2);
 private:
-    string first = "It is morning on the first continent\n";
-    string second = "The population is waking up on the first continent\n";
-    string third = "On the other continent population are sleeping\n";
-    string forth = "It is already night , on the first continent people have to go sleep\n";
-    string fifth = "On the 2nd continent population are waking up\n";
-    string sixth = "Day is going good!\n";
-    string seventh = "Nothing special, it is evening and on the second continent population is going to sleep\n";
-    string eight = "It is going again!\n";
+    const char* first = "It is morning on the first continent\n";
+    const char* second = "The population is waking up on the first continent\n";
+    const char* third = "On the other continent population are sleeping\n";
+    const char* forth = "It is already night , on the first continent people have to go sleep\n";
+    const char* fifth = "On the 2nd continent population are waking up\n";
+    const char* sixth = "Day is going good!\n";
+    const char* seventh = "Nothing special, it is evening and on the second continent population is going to sleep\n";
+    const char* eight = "It is going again!\n";
 };
 
 #endif // DAYCYCLE_H
--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main () {
     DayCycle daycycle;
     std::ofstream file("data.txt");
     if (file.is_open())
-        daycycle(file, earth, 10);
+        daycycle(file, earth);
     file.close();
 }
